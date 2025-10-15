d8353dc02758fb925130e0183b09751b81472928
Haku Clement <ultimatejustin20@gmail.com>
2025-10-15 22:12:17 +0100

    optimization

--- nova_backend.py
+++ nova_backend.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
-from passlib.context import CryptContext
 from jose import jwt, ExpiredSignatureError, JWTError
 from dotenv import load_dotenv
 import pendulum
